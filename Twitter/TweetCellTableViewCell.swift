9ed08f650b430e99698bf3384f871316b341c6eb
Azael Zamora <azael.zamora12@gmail.com>
2020-09-17 22:15:08 -0700

    twitter part 1

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Azael Zamora on 9/17/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
